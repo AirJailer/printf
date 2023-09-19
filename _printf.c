52f03bebb342a9f9d4c6d72d4f0ca2c31b177bd3
“AirJailer” <airjailer1@gmail.com>
2023-09-19 08:49:06 -0700

    main fun update and task 2

--- _printf.c
+++ _printf.c
@@ -1,48 +1,43 @@
 #include "main.h"
 
 /**
- * _printf - function that produces output according to a format.
- * @format: format to print
- * Return: the number of characters printed
+ * _printf - is a function tha produces output according to a format.
+ * @format: format to look for and print.
+ * Return: The length of chracters printed.
  */
+
 int _printf(const char *format, ...)
 {
+	format_match m[] = {
+		{"%s", printf_string}, {"%c", printf_char}, {"%%", printf_percent},
+		{"%i", printf_int}, {"%d", printf_dec}, {"%b", printf_binary},
+		, {"%u", printf_unsigned}, {"%p", printf_pointer}, {"%p", printf_oct}
+		, {"%p", printf_hex}, {"%p", printf_HEX_}
+	};
+
 	va_list args;
-	int i = 0, len = 0;
+	int i = 0, j, len = 0;
 
 	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	if (format == NULL || (format[0] == '&' && format[1] == '\0'))
 		return (-1);
+
+Here:
 	while (format[i] != '\0')
 	{
-		if (format[i] == '%')
+		j = 10;
+		while (j >= 0)
 		{
-			i++;
-			if (format[i] == 'c')
+			if (m[j].id[0] == format[i] && m[j].id[1] == format[1 + 1])
 			{
-				char c = va_arg(args, int);
-
-				len += _putchar(c);
+				len += m[j].f(args);
+				i = i + 2;
+				goto Here;
 			}
-			else if (format[i] == '%')
-			{
-				len += printf_percent();
-			}
-			else if (format[i] == 'i')
-				len += printf_int(args);
-			else if (format[i] == 'd')
-				len += printf_dec(args);
-			else if (format[i] == 's')
-			{
-				char *s = va_arg(args, char *);
-
-				len += printf_string(s);
-			}
+			j--;
 		}
-		else
-		{
-			len += _putchar(format[i]);
-		}
+		_putchar(format[i]);
+		len++;
 		i++;
 	}
 	va_end(args);
--- main.h
+++ main.h
@@ -2,16 +2,38 @@
 #define MAIN_H
 
 
+#include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <string.h>
+
+/**
+ * struct format - match the conversion specifiers for printf
+ * @id: type char pointer of the specifier
+ * @f: type pointer to function that handles the conversion
+ */
+
+typedef struct format
+{
+	char *id;
+	int (*f)();
+} format_match;
 
 
 int _putchar(char c);
 int _strlen(char *s);
 int printf_string(char *s);
+int printf_char(va_list args);
 int printf_percent(void);
 int printf_int(va_list args);
 int printf_dec(va_list args);
+int printf_binary(va_list args);
+int printf_unsigned(va_list args);
+int printf_pointer(va_list args);
+int printf_hex_sec(unsigned long int num);
+int printf_hex(va_list args);
+int printf_oct(va_list args);
+int printf_HEX_(va_list args);
 int _printf(const char *format, ...);
 
 #endif

7cf4b35b6286e6af529ada032ec1201e6e0494c0
“AirJailer” <airjailer1@gmail.com>
2023-09-19 18:17:57 -0700

    fix

--- _printf.c
+++ _printf.c
@@ -9,23 +9,21 @@
 int _printf(const char *format, ...)
 {
 	format_match m[] = {
-		{"%s", printf_string}, {"%c", printf_char}, {"%r", printf_reverse}
+		{"%s", printf_string}, {"%c", printf_char}, {"%r", printf_reverse},
 		{"%i", printf_int}, {"%d", printf_dec}, {"%b", printf_binary},
 		{"%u", printf_unsigned}, {"%p", printf_pointer}, {"%o", printf_oct},
 		{"%x", printf_hex}, {"%X", printf_HEX_}, {"%S", printf_special_string}
 	};
-
 	va_list args;
 	int i = 0, j, len = 0;
 
 	va_start(args, format);
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
-
 Here:
 	while (format[i] != '\0')
 	{
-		j = 10;
+		j = 11;
 
 		if (format[i] == '%' && format[i + 1] == '%')
 		{
